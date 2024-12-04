adf9cdd9ba1a3d5037f6a64e7d3a552144f62298
LP <louisphilipshahim@gmail.com>
2024-12-04 06:45:57 +0200

    fixing testing issues

--- test/advent_of_code_test.dart
+++ test/advent_of_code_test.dart
@@ -1,8 +1,11 @@
-import 'package:advent_of_code/advent_of_code.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+}
+
+int calculate() {
+  return 6 * 7;
 }
